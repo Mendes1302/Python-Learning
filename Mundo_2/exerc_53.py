c61685cf4da0467c48e32d2b8a7deb4425059b31
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-10-17 14:23:43 -0300

    Update exerc_53.py

--- Mundo_2/exerc_53.py
+++ Mundo_2/exerc_53.py
@@ -1,7 +1,9 @@
-frase = str(input("Digite uma frase: ")).strip()
-var = 0
+var = 0  
+frase = ''.join(str(input("Digite uma frase: ")).split())
+
+print(frase)
 for i in range(1, (len(frase))+1):
-  if frase[i-1] == frase[len(frase)-i]:
-    var = var + 1
+    if frase[i-1] == frase[len(frase)-i]:
+        var = var + 1
 if var == len(frase):
-  print("Frase e polindromo")
+    print("Frase e polindromo")
