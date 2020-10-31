c4c2ab52499e05d4e8352ee72bf2106ab0c466d5
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-10-31 15:10:40 -0300

    Create exerc_71.py

---
+++ Mundo_2/exerc_71.py
@@ -0,0 +1,23 @@
+valor = int(input("Digite o valor para sacar:"))
+resto = 1
+cont50 = cont20 = cont10 = cont1 = 0
+print("O valor de R$ {} terá".format(valor))
+while resto != 0:
+    if valor >= 50:
+        resto = valor % 50
+        cont50 = valor / 50 
+    elif valor >= 20 and valor < 50:
+        resto = valor % 20
+        cont20 = valor / 20
+    elif valor >= 10 and valor < 20:
+        resto = valor % 10
+        cont10 =valor / 10
+    elif valor >= 1and valor < 10:
+        resto = valor % 1
+        cont1 = valor / 1
+    valor = resto
+print("{:.0f} x R$ 50 reais\n{:.0f} x R$ 20 reais".format(cont50, cont20))
+print("{:.0f} x R$ 10 reais\n{:.0f} x R$ 1 reais".format(cont10, cont1))
+    
+    
+ 
