83fda7310bcbc6684576a578891a0ae93381ffcc
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-17 19:32:16 -0300

    Create exerc_79.py

---
+++ Mundo_3/exerc_79.py
@@ -0,0 +1,17 @@
+control = 'S'
+cont = 0
+array = list()
+print("\n" , "="*30, "BEGIN", "="*30)
+while control in "Ss":
+    value = int(input("\nDigite um valor: "))
+    if cont == 0:
+        array.append(value)
+    else:
+        if(value in array):
+                print("VALOR DUPLICADO")
+        else:
+            array.append(value)
+    cont = cont + 1    
+    control  = str(input("\nDeseja continuar [S/N]: "))
+    print(array)
+print("\n" , "="*30, "END", "="*30)
