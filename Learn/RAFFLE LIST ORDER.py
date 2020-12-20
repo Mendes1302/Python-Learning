b763d32f6c32b78742e4531ef91eb13f387bfd8c
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 14:18:31 -0300

    Create RAFFLE LIST ORDER.py

---
+++ Learn/RAFFLE LIST ORDER.py
@@ -0,0 +1,9 @@
+from random import shuffle 
+
+nome1 = str(input('Digite o nome do primeiro aluno:'))
+nome2 = str(input('Digite o nome do segundo aluno:'))
+nome3 = str(input('Digite o nome do terceiro aluno:'))
+nome4 = str(input('Digite o nome do quarto aluno:'))
+lista = [nome1, nome2, nome3, nome4]
+shuffle(lista)
+print(f'A ordem de apresentacao sera\n{lista}')
