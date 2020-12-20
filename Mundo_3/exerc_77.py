50377b25f3044176410734c5703d70076f212920
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 15:14:32 -0300

    Delete exerc_77.py

--- Mundo_3/exerc_77.py
+++
@@ -1,7 +0,0 @@
-tupla = ('Lucas', 'Lapis', 'Notebook', 'Faculdade', 'Python')
-
-for palavra in tupla: 
-    print(f'\nNa palavra {palavra.upper()} temos ', end='')
-    for letras in palavra:
-        if letras.lower() in 'aeiou':
-            print(f'{letras}', end=' ')
