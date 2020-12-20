907e919ceaa7b3c1f5b4e59469f9751b1d641c9a
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 14:54:21 -0300

    Delete exerc_ 48.py

--- Mundo_2/exerc_ 48.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-cont = 0
-for i in range (1, 501):
-  if (i % 2) != 0 and (i % 3) == 0:
-    cont = cont + 1
-    soma = soma + i
-print(f"O valor da soma dos {cont} valores eh {soma}")
-    
