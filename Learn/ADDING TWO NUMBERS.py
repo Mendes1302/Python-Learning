3ad8778e934beb7a7ec848d489cf9ab684cba710
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 14:42:20 -0300

    Update ADDING TWO NUMBERS.py

--- Learn/ADDING TWO NUMBERS.py
+++ Learn/ADDING TWO NUMBERS.py
@@ -1,6 +1,6 @@
 print("-"*30,'SOMA',"-"*30)
 
-a = int(input('\nDigite o primeiro numero:'))
-b = int(input('\nDigite o primeiro numero:'))
+a = int(input('\nDigite o PRIMEIRO numero:'))
+b = int(input('\nDigite o SEGUNDO numero:'))
 soma = a + b
 print(f'\n\nA soma entre {a} e {b} é {soma}')
