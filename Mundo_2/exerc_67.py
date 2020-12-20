f0de16f68643f6fab24f1b0cb95bc292a9470e73
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 15:11:04 -0300

    Delete exerc_67.py

--- Mundo_2/exerc_67.py
+++
@@ -1,7 +0,0 @@
-escolha = 1
-while escolha > 0:
-    escolha = int(input("Digite [ -1 ] para sair ou um numero para saber a tabuada:"))
-    print("="*30)
-    for i in range(0,11):
-        print("{} x {} = {}".format(escolha, i, i*escolha))
-    print("="*30)
