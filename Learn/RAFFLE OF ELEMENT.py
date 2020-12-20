a4063857ffdd1baca633258538d2824fa9ae754e
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 14:15:42 -0300

    Update RAFFLE OF ELEMENT.py

--- Learn/RAFFLE OF ELEMENT.py
+++ Learn/RAFFLE OF ELEMENT.py
@@ -5,3 +5,5 @@ nome2 = str(input('Digite o nome do segundo aluno:'))
 nome3 = str(input('Digite o nome do terceiro aluno:'))
 nome4 = str(input('Digite o nome do quarto aluno:'))
 lista = [nome1, nome2, nome3, nome4]
+
+print(f'O nome sorteado foi {choice(lista)}')
