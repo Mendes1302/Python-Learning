9936844082bfde0cc0c86c6abe47be2b6e62eaba
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2020-12-20 14:15:23 -0300

    Create RAFFLE OF ELEMENT.py

---
+++ Learn/RAFFLE OF ELEMENT.py
@@ -0,0 +1,7 @@
+from random import choice 
+
+nome1 = str(input('Digite o nome do primeiro aluno:'))
+nome2 = str(input('Digite o nome do segundo aluno:'))
+nome3 = str(input('Digite o nome do terceiro aluno:'))
+nome4 = str(input('Digite o nome do quarto aluno:'))
+lista = [nome1, nome2, nome3, nome4]
